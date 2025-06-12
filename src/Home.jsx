a843102c47ca36097cbc39f2cf1831f91765df1b
codewitch-honey-crisis <gaztoast@gmail.com>
2025-06-11 23:40:02 -0700

    navigation

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,9 @@ import './App.css';
 function Home() {
   return (
     <>
+      <a href=",/docs/index.html">Documentation</a><br />
+      <a href="https://github.com/codewitch-honey-crisis/gfx">GitHub Repo</a>
+      <h1>GFX</h1>
       <a href="./header/index.html">Header generator</a><br />
       <a href="./icon-pack/index.html">Icon pack generator</a>
     </>
